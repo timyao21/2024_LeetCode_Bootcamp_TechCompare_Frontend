d2e88f9f16436d9c448d088254d2205d50829967
BellalZY <949424279@qq.com>
2024-04-23 16:58:37 -0400

    merge

--- techcompare_frontend/src/index.js
+++ techcompare_frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './app/store'
-import { Provider } from 'react-redux'
 
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
